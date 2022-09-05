276325d8e4ef8419e1219fafc90edd395f0e52f4
Chris Deboli <christopher2002buffon@gmail.com>
2022-09-05 12:10:18 +0200

    Creazione enum "Choice"

---
+++ BlackjackLib/Enum/Choice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackjackLib.Enum
+{
+    public enum Choice
+    {
+        Hit,
+        Stay
+    }
+}
